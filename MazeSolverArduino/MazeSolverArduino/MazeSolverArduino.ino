62916b12b776b7f51dea9584a3d24b7f89f769ba
Matija Kralj <mkralj@foi.hr>
2018-10-23 17:06:37 +0200

    Inicijalno postavljanje arduino i android aplikacije. Verzija android studia updateana: 23.10.2018

---
+++ MazeSolverArduino/MazeSolverArduino/MazeSolverArduino.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		MazeSolverArduino.ino
+ Created:	10/23/2018 5:00:24 PM
+ Author:	Matija
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
