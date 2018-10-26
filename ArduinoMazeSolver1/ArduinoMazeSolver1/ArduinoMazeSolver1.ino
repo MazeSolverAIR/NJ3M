b453bc16cfe777ed11a6763f0094634e58c14016
Matija Kralj <mkralj@foi.hr>
2018-10-26 13:36:12 +0200

    Dodana logika za izračunavanje trajanja rotacije kod različitih brzina i stupnjeva

--- ArduinoMazeSolver1/ArduinoMazeSolver1/ArduinoMazeSolver1.ino
+++ ArduinoMazeSolver1/ArduinoMazeSolver1/ArduinoMazeSolver1.ino
@@ -1,63 +1,63 @@
-/**
- * \par Copyright (C), 2012-2016, MakeBlock
- * @file    DCMotorDriverTest.ino
- * @author  MakeBlock
- * @version V1.0.0
- * @date    2015/09/09
- * @brief   Description: this file is sample code for Me DC motor device.
- *
- * Function List:
- *    1. void MeDCMotor::run(int16_t speed)
- *    2. void MeDCMotor::stop(void)
- *
- * \par History:
- * <pre>
- * <Author>     <Time>        <Version>      <Descr>
- * Mark Yan     2015/09/09    1.0.0          rebuild the old lib
- * </pre>
- */
-#include "MeOrion.h"
+#include "MeMCore.h"
 
 MeDCMotor motor1(M1);
 
 MeDCMotor motor2(M2);
 
-uint8_t motorSpeed = 127;
+uint8_t brzinaMotora = 127;
 
-//TODO: Varijabla koja pamti omjer između vremena potrebnog za okretanje kod brzine = 127 za 90° i trenutne brzine i koristi se za delay
-//TODO: implementirati v = s / t ; s = stupnjevi , v = brzina, t = delay
+const uint8_t brzinaKodOmjera = 127;
+
+float omjerConst = 0;
 
 void setup()
 {
+  const float stupnjeviZaOmjer = 90;
+  const float delayZaOmjer = 472;
+  omjerConst = stupnjeviZaOmjer / delayZaOmjer;
 }
 
 void loop()
 {
-  Skreni('l',motorSpeed); 
+  Skreni('l', 360, brzinaMotora); 
 
-  delay(450);
+  ZaustaviMotore();
 
-  PokreniMotore(0);
-
-  delay(1000);
+  delay(2000);
 }
 
 void PokreniMotore(uint8_t motorSpeed)
 {
-  motor1.run(-motorSpeed); /* value: between -255 and 255. */
+  motor1.run(-motorSpeed);
   motor2.run(motorSpeed);
 }
 
-void Skreni(char smijer, uint8_t motorSpeed)
+void ZaustaviMotore()
 {
+  motor1.stop(); 
+  motor2.stop();
+}
+
+void Skreni(char smijer, int stupnjevi, uint8_t motorSpeed)
+{
+  float vrijemeTrajanjaRotacije = IzracunajVrijemeRotacije(stupnjevi, motorSpeed);
   if(smijer == 'l')
   {
-    motor1.run(motorSpeed); /* value: between -255 and 255. */
+    motor1.run(motorSpeed);
     motor2.run(motorSpeed);
   }
   else
   {
-    motor2.run(-motorSpeed); /* value: between -255 and 255. */
+    motor2.run(-motorSpeed);
     motor1.run(-motorSpeed);
   }
+
+  delay((int)vrijemeTrajanjaRotacije);
+}
+
+float IzracunajVrijemeRotacije(int stupnjevi, uint8_t motorSpeed)
+{
+  float vrijeme = 0;
+  
+  return vrijeme = stupnjevi/((motorSpeed/brzinaKodOmjera)*omjerConst);
 }
