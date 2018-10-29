5a0e623f66000d2868b8d456a9b733fe98133ff7
Matija Kralj <mkralj@foi.hr>
2018-10-29 16:40:05 +0100

    Osposobljen rad s gumbom

--- ArduinoMazeSolver1/MazeSolver1/MazeSolver1.ino
+++ ArduinoMazeSolver1/MazeSolver1/MazeSolver1.ino
@@ -1,31 +1,76 @@
 
 #include "MeMCore.h"
+#include "Arduino.h"
 
-uint16_t brzinaKretanja = 200;
+uint16_t brzinaKretanja = 100;
 
 MeDCMotor leftMotor(M1);
 MeDCMotor rightMotor(M2);
 
 MeUltrasonicSensor ultraSonic(3);
 
-MeBluetooth
+Me4Button button = Me4Button();
 
+uint8_t modRadnje = -1;
+bool stisnutGumb = true;
 
-float omjerSpeed = 0.0f;
-float vrijemeOkretanjeZaDevedeset = 0.0f;
+int state = 0;
 
 void setup() 
 {
+	button.setpin(A7);
+
+	Serial.begin(38400);
 }
 
 void loop() 
 {
-	Skreni('l', 90, brzinaKretanja); 
-	ZaustaviMotore();
 
-	delay(2000);
+	if (button.pressed() && stisnutGumb)
+	{
+		stisnutGumb = false;
+		modRadnje++;
 
-	/*if (ultraSonic.distanceCm() > 20)
+		if (modRadnje > 2)
+			modRadnje = 0;
+	}
+	if (!button.pressed())
+	{
+		stisnutGumb = true;
+	}
+
+	switch (modRadnje)
+	{
+	case 0:
+		IzbjegavajPrepreke();
+		break;
+	case 1:
+		UpaliBlueTooth();
+		break;
+	case 2:
+		ZaustaviMotore();
+	default:	
+		ZaustaviMotore();
+		break;
+	}
+
+}
+
+void UpaliBlueTooth()
+{
+	if (Serial.available() > 0) {
+		state = Serial.read();
+	}
+
+	if (state == '1') {
+		Kreni(brzinaKretanja);
+		state = 0;
+	}
+}
+
+void IzbjegavajPrepreke()
+{
+	if (ultraSonic.distanceCm() > 20)
 	{
 		Kreni(150);
 	}
@@ -33,8 +78,8 @@ void loop()
 	{
 		ZaustaviMotore();
 		delay(1000);
-		
-	}*/
+
+	}
 }
 
 void Kreni(uint16_t brzinaKretanja)
