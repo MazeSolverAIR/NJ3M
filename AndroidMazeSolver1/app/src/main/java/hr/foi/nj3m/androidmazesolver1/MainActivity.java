4c34fb1f62094f94daef185911286efe342bff3f
Nikola Petanjek <npetanjek@foi.hr>
2018-11-07 16:50:22 +0100

    Rješavanje konflikata

--- AndroidMazeSolver1/app/src/main/java/hr/foi/nj3m/androidmazesolver1/MainActivity.java
+++ AndroidMazeSolver1/app/src/main/java/hr/foi/nj3m/androidmazesolver1/MainActivity.java
@@ -1,22 +1,21 @@
 package hr.foi.nj3m.androidmazesolver1;
 
-<<<<<<< HEAD
+
 import android.bluetooth.BluetoothAdapter;
-=======
+
 import android.content.Intent;
->>>>>>> master
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-<<<<<<< HEAD
+
 import android.widget.TextView;
 
 import com.example.bluetooth.Bluetooth;
-=======
+
 import android.widget.ImageButton;
 import android.widget.Toast;
->>>>>>> master
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,29 +25,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
+
         Button btnEnableDisableBT = (Button) findViewById(R.id.btnEnableDisableBT);
         Button btnDiscover = (Button) findViewById(R.id.btnDiscover);
 
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        final Bluetooth bluetooth = new Bluetooth(this,mBluetoothAdapter);
+        //final Bluetooth bluetooth = new Bluetooth(this,mBluetoothAdapter);
 
         btnEnableDisableBT.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bluetooth.enableDisableBluetooth();
+                //bluetooth.enableDisableBluetooth();
             }
         });
 
         btnDiscover.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bluetooth.discover();
+                //bluetooth.discover();
                 TextView deviceName = (TextView) findViewById(R.id.tvDeviceName);
                 TextView deviceAddress = (TextView) findViewById(R.id.tvDeviceAddress);
             }
         });
-=======
 
         final ImageButton tipkaBluetooth = (ImageButton) findViewById(R.id.btnBluetooth);
         tipkaBluetooth.setOnClickListener(new View.OnClickListener() {
@@ -63,6 +61,5 @@ public class MainActivity extends AppCompatActivity {
     public void openListOfDevices(){
         Intent intent = new Intent(this,ListOfDevices.class);
         startActivity(intent);
->>>>>>> master
     }
 }
