088d7cb62030b030e3d5bbb215545add374c8cf6
Nikola Petanjek <npetanjek@foi.hr>
2018-11-07 23:31:37 +0100

    U klasi Bluetooth dodane metode za uključivanje Bluetootha, pronalazak dostupnih uređaja te uparivanje uređaja. Dodan ListView koji sadrži popis dostupnih uređaja. Potrebno je još srediti dizajn ListViewa u aktivnosti popisa uređaja.

--- AndroidMazeSolver1/app/src/main/java/hr/foi/nj3m/androidmazesolver1/ListOfDevices.java
+++ AndroidMazeSolver1/app/src/main/java/hr/foi/nj3m/androidmazesolver1/ListOfDevices.java
@@ -1,13 +1,57 @@
 package hr.foi.nj3m.androidmazesolver1;
 
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListView;
 
-public class ListOfDevices extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class ListOfDevices extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    Bluetooth bluetooth;
+    public ArrayList<BluetoothDevice> mBTDevices = new ArrayList<>();
+    public DeviceListAdapter mDeviceListAdapter;
+    ListView lvNewDevices;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_of_devices);
+
+        lvNewDevices = (ListView) findViewById(R.id.lvNewDevices);
+        mBTDevices = new ArrayList<>();
+
+        bluetooth = new Bluetooth(this, MainActivity.mBluetoothAdapter, mBroadcastReceiver);
+        bluetooth.discover();
+    }
+    
+
+
+    private BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+            if(action.equals(BluetoothDevice.ACTION_FOUND)){
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                mBTDevices.add(device);
+                mDeviceListAdapter = new DeviceListAdapter(context, R.layout.device_adapter_view, mBTDevices);
+                lvNewDevices.setAdapter(mDeviceListAdapter);
+
+                lvNewDevices.setOnItemClickListener(ListOfDevices.this);
+            }
+        }
+    };
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        bluetooth.createBond(mBTDevices, position);
     }
 }
