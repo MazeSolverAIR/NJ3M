a8e1a6fbe225c7914f8043ad1163da9fae87cda6
Josip Marijanović <jmarijano@foi.hr>
2018-12-29 19:12:35 +0100

    Implementiraj navigation drawer.

--- AndroidMazeSolver1/app/src/main/java/hr/foi/nj3m/androidmazesolver1/MainActivity.java
+++ AndroidMazeSolver1/app/src/main/java/hr/foi/nj3m/androidmazesolver1/MainActivity.java
@@ -3,29 +3,105 @@ package hr.foi.nj3m.androidmazesolver1;
 
 import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import hr.foi.nj3m.interfaces.IWireless;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
-    private static final String TAG = "MainActivity";
-    public static BluetoothAdapter mBluetoothAdapter;
-    public Button mButton;
-    IWireless iWireless;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Fragment fragment = new ConnectionTypeSelectionFragment();
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.fragment_container, fragment);
-        transaction.commitAllowingStateLoss();
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        drawer = findViewById(R.id.drawer_layout);
+        final NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            new Handler().post(new Runnable() {
+                @Override
+                public void run() {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new GalleryFragment()).commit();
+                    navigationView.setCheckedItem(R.id.nav_gallery);
+                }
+            });
+        }
+        new Handler().post(new Runnable() {
+            @Override
+            public void run() {
+                Fragment fragment = new ConnectionTypeSelectionFragment();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container, fragment);
+                transaction.commitAllowingStateLoss();
+            }
+        });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.nav_gallery:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .setCustomAnimations(R.anim.enter_left_to_right,R.anim.exit_left_to_right,R.anim.enter_right_to_left,R.anim.exit_right_to_left)
+                        .replace(R.id.fragment_container,new GalleryFragment())
+                        .commit();
+                break;
+            case R.id.nav_add_photo:
+                getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.enter_left_to_right,R.anim.exit_left_to_right,R.anim.enter_right_to_left,R.anim.exit_right_to_left).replace(R.id.fragment_container,new AddFragment()).commit();
+                break;
+            case R.id.nav_info:
+                Toast.makeText(this,"Info",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_sensor_selection:
+                getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.enter_left_to_right,R.anim.exit_left_to_right,R.anim.enter_right_to_left,R.anim.exit_right_to_left).replace(R.id.fragment_container,new SensorSelectionFragment()).commitAllowingStateLoss();
+                break;
+
+        }
+        new Handler().post(new Runnable() {
+            @Override
+            public void run() {
+                drawer.closeDrawer(GravityCompat.START);
+            }
+        });
+
+        return true;
+    }
+
+    @Override
+    public void onBackPressed(){
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();}
     }
 }
