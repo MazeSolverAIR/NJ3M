e0950ceae6939f49c63a4a2330c38a9311c49f93
mleskovic <mleskovic@foi.hr>
2019-01-14 12:45:54 +0100

    Dodan responzivan dizajn i za loading screen sa animacijom.

--- AndroidMazeSolver1/app/src/main/java/hr/foi/nj3m/androidmazesolver1/loadingScreen.java
+++ AndroidMazeSolver1/app/src/main/java/hr/foi/nj3m/androidmazesolver1/loadingScreen.java
@@ -1,21 +1,45 @@
 package hr.foi.nj3m.androidmazesolver1;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class loadingScreen extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 1000;
+    private static int SPLASH_TIME_OUT = 2500;
+
+    Animation fromBottom;
+    Animation fromTop;
+    Animation fromLeft;
+    Animation fromRight;
+    ImageView logo;
+    ImageView tekstMazeSolver;
+    ImageView tekstMakeBlock;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading_screen);
 
-        //OtvaranjeGlavneForme
+        logo = (ImageView) findViewById(R.id.logoNMJ);
+        tekstMakeBlock = (ImageView) findViewById(R.id.txtMazeBlock);
+        tekstMazeSolver = (ImageView) findViewById(R.id.txtMazeSolver);
 
+        fromTop = AnimationUtils.loadAnimation(this,R.anim.from_top);
+        fromBottom = AnimationUtils.loadAnimation(this,R.anim.from_bottom);
+        fromLeft = AnimationUtils.loadAnimation(this,R.anim.enter_left_to_right);
+        fromRight = AnimationUtils.loadAnimation(this,R.anim.enter_right_to_left);
+
+        logo.setAnimation(fromTop);
+        tekstMazeSolver.setAnimation(fromLeft);
+        tekstMakeBlock.setAnimation(fromRight);
+
+        //OtvaranjeGlavneForme
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
