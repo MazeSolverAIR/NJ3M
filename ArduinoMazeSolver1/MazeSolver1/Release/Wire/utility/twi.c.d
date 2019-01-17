2900b31016aedd48b8cbe5bea1e811e2481a7a5c
Matija Kralj <mkralj@foi.hr>
2019-01-17 16:10:56 +0100

    Initial commit for android development

--- ArduinoMazeSolver1/MazeSolver1/Release/Wire/utility/twi.c.d
+++
@@ -1,6 +0,0 @@
-C:\Users\Matija\AppData\Local\Temp\VMBuilds\MazeSolver1\uno\Release\Wire\utility\twi.c.o: \
- D:\ArduinoIDE\Arduino\hardware\arduino\avr\libraries\Wire\src\utility\twi.c \
- D:\ArduinoIDE\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
- D:\ArduinoIDE\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
- D:\ArduinoIDE\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
- D:\ArduinoIDE\Arduino\hardware\arduino\avr\libraries\Wire\src\utility\twi.h
